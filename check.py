66f721764f6f15e840dba916b180eb4fe18307ef
Alejandro JNM <alejandrojnm@gmail.com>
2019-09-27 08:17:57 -0400

    Update blank.yml and check if the VM exist if not we create
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

--- check.py
+++ check.py
@@ -1,6 +1,14 @@
 from civo import Civo
 
+hostname_default = 'gitops-civo.example.com'
 
 civo = Civo()
-template = civo.templates.lists(filter='code:debian-stretch')[0]['id']
-print(template)
+size_id = civo.size.search(filter='name:g2.xsmall')[0]['name']
+template = civo.templates.search(filter='code:debian-stretch')[0]['id']
+search_hostname = civo.instances.search(filter='hostname:{}'.format(hostname_default))
+
+if not search_hostname:
+    civo.instances.create(hostname='text.example.com', size=size_id,
+                          region='lon1', template_id=template,
+                          public_ip='true', ssh_key='alejandrojnm')
+    print(template)
