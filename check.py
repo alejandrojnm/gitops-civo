517770b2f8d30f36dbdcd4e307dda626a981b212
Alejandro JNM <alejandrojnm@gmail.com>
2019-09-27 08:20:57 -0400

    Update check.py
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

--- check.py
+++ check.py
@@ -6,9 +6,10 @@ civo = Civo()
 size_id = civo.size.search(filter='name:g2.xsmall')[0]['name']
 template = civo.templates.search(filter='code:debian-stretch')[0]['id']
 search_hostname = civo.instances.search(filter='hostname:{}'.format(hostname_default))
+ssh_id = civo.ssh.search(filter='name:alejandrojnm')[0]['id']
 
 if not search_hostname:
     civo.instances.create(hostname='text.example.com', size=size_id,
                           region='lon1', template_id=template,
-                          public_ip='true', ssh_key='alejandrojnm')
+                          public_ip='true', ssh_key_id=ssh_id)
     print(template)
